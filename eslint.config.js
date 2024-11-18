eaebd492a127ec666f2a74f0488c27a623557f55
Jonathan <many.d.r.o.2001@hotmail.com>
2024-11-17 21:14:30 -0500

    Project initializated

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error'
+    },
+    languageOptions: { globals: globals.browser }
+  },
+  pluginJs.configs.recommended
+];
